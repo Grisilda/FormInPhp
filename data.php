d15433f7a7f93a7979b39e795217633e6be4099c
Grisilda <46270641+Grisilda@users.noreply.github.com>
2019-07-27 23:52:24 +0200

    Update data.php

--- data.php
+++ data.php
@@ -6,10 +6,16 @@
   $query="Select role from user where ussername = "." '$ussername'"." and password = "." '$password'";
   $data=mysqli_query($connect,$query);
   $row=mysqli_fetch_array($data);
-  if($row[0]==0){
+  if($row[0]==2){
   	header('Location: worker.html');
   }
-  else{
+  else if($row[0]==1){
   	header('Location: admin.html');
+  }
+  else if($row[0]==0){
+  	echo "<script type='text/javascript'>
+  	alert('There was a problem with your credentials!');
+  	window.location.href='logimi2.html';
+  	</script>";
   }
 ?>
